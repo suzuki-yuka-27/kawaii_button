2f7c4fd5d7c8cf4ccdde80070ac3eeed9249ae38
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-06-03 23:58:23 +0900

    [add]add user function

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'buttons#update'
+
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_seeeions#destroy'
+
+  resources :users, only: %i[new create] do
+    resource :mypage
+  end
+  resources :user_seeeions, only: %i[new create destroy]
 end
