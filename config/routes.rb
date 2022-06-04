57997ddc660286e6a68385f83934c4d4d299de75
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-06-04 15:25:16 +0900

    [create]create config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  root 'buttons#update'
-
-  get '/login', to: 'user_sessions#new'
-  post '/login', to: 'user_sessions#create'
-  delete '/logout', to: 'user_seeeions#destroy'
-
-  resources :users, only: %i[new create] do
-    resource :mypage
-  end
-  resources :user_seeeions, only: %i[new create destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
